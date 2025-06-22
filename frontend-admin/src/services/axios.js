5ccdd29d21a18213956e2651507883abfab51bc9
Valon Sopa <fazlibeqir@gmail.com>
2025-06-22 16:45:20 +0200

    url change

--- frontend-admin/src/services/axios.js
+++ frontend-admin/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../stores/auth'
 
 const api = axios.create({ 
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://deliverxy.local/api",
 
 })
 
