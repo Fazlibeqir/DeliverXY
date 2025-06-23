8793cdb05cd7fb0d3929cc20f06263aa5da3fad5
Valon Sopa <fazlibeqir@gmail.com>
2025-06-24 01:19:42 +0200

    Fixing enviroments ports and urls

--- backend/src/main/java/com/deliverXY/backend/configs/CorsConfig.java
+++ backend/src/main/java/com/deliverXY/backend/configs/CorsConfig.java
@@ -5,20 +5,28 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CorsFilter;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       
         CorsConfiguration config=new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173",
-        "http://frontend-admin:80","http://deliverxy.local"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
