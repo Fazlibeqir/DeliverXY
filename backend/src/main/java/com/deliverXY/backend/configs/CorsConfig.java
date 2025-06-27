64b02ed70018365f21bc61c47117fa0811a42d0c
Valon Sopa <fazlibeqir@gmail.com>
2025-06-27 19:21:11 +0200

    config fixes

--- backend/src/main/java/com/deliverXY/backend/configs/CorsConfig.java
+++ backend/src/main/java/com/deliverXY/backend/configs/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CorsFilter;
 
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,17 +15,17 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins}")
-    private String allowedOrigins;
+  //  @Value("${app.cors.allowed-origins}")
+    //private String allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
        
         CorsConfiguration config=new CorsConfiguration();
         config.setAllowCredentials(true);
-        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+       // List<String> origins = Arrays.asList(allowedOrigins.split(","));
        
-        config.setAllowedOrigins(origins);
+        config.setAllowedOrigins(Arrays.asList("http://13.48.56.2:3000", "http://ec2-13-48-56-2.eu-north-1.compute.amazonaws.com:3000"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 
