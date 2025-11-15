c8600cfb789796f4d0e7240ad6af1e945ee4b9b0
eminiburak <fazlibeqir@gmail.com>
2025-11-15 19:14:48 +0100

    cleanup: removed legacy backend code in preparation for NewCode refactor

--- backend/src/main/java/com/deliverXY/backend/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.deliverXY.backend.service;
-
-import com.deliverXY.backend.models.dto.ReqRes;
-
-public interface AuthService {
-    ReqRes signUp(ReqRes registrationRequest);
-    ReqRes signIn(ReqRes loginRequest);
-    ReqRes refreshToken(ReqRes refreshTokenRequest);
-}
--- backend/src/main/java/com/deliverXY/backend/service/CustomUserDetailsService.java
+++
@@ -1,6 +0,0 @@
-package com.deliverXY.backend.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-}
